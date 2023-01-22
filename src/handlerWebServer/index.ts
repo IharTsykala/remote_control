aad1d3c57213b2b8faa4e70fcaf42731bc17277c
IharTsykala <ihatsykala@kitrum.com>
2023-01-22 19:58:44 +0600

    feat: add try catch for errors

--- src/handlerWebServer/index.ts
+++ src/handlerWebServer/index.ts
@@ -3,11 +3,16 @@ import { ACTIONS_MOUSE_CLICK } from './constants'
 import { IHandlerWebServer } from '../interfaces'
 
 export const handlerWebServer = async ({ frontData }: IHandlerWebServer) => {
-  const [command, pixels = 0, additionalPixels = 0] = frontData.split(' ')
+  try {
+    const [command, pixels = 0, additionalPixels = 0] = frontData.split(' ')
 
-  let response
-  if (ACTIONS_MOUSE_CLICK[command]) {
-    response = await ACTIONS_MOUSE_CLICK[command](Number(pixels), Number(additionalPixels))
+    let response
+    if (ACTIONS_MOUSE_CLICK[command]) {
+      response = await ACTIONS_MOUSE_CLICK[command](Number(pixels), Number(additionalPixels))
+    }
+    return response ?? command
+  } catch (e) {
+    console.log(e)
+    return 'Error Web Socket'
   }
-  return response ?? command
 }
